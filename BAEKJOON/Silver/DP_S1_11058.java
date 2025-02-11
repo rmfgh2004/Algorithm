ae8eb7c4143ce2d856dc9b65a98094f7a26f5b08
rmfgh2004 <76586854+rmfgh2004@users.noreply.github.com>
2025-02-11 23:04:03 +0900

    크리보드 풀이

---
+++ BAEKJOON/Silver/DP_S1_11058.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    long[] dp = new long[N + 1];
+
+    for (int i = 1; i <= N; i++) {
+      dp[i] = dp[i - 1] + 1;
+
+      // 6이전은 그냥 AAA ACV 라고 가정하고 시작
+      if (i > 6) {
+        // ACV -> dp[i - 3] * 2, ACVV -> dp[i - 4] * 3, ACVVV -> dp[i - 5] * 4
+        for (int j = 3; j <= 5; j++) {
+          dp[i] = Math.max(dp[i], dp[i - j] * (j - 1));
+        }
+      }
+    }
+
+    System.out.print(dp[N]);
+  }
+}
