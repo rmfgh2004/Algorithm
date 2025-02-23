902ba0234f8a0e407c36215bbc202d1abf36807f
rmfgh2004 <76586854+rmfgh2004@users.noreply.github.com>
2025-02-23 09:52:48 +0000

    기본 코드 구조 파일 생성

---
+++ SW Expert Academy/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        int turn = 1;
+        while (T-- > 0) {
+
+            turn++;
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
