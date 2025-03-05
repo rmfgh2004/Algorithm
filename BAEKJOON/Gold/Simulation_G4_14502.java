ae071b1b13bf96e175097e8dbb3890a701f6ee61
rmfgh2004 <76586854+rmfgh2004@users.noreply.github.com>
2025-03-05 14:16:43 +0000

    연구소 풀이

---
+++ BAEKJOON/Gold/Simulation_G4_14502.java
@@ -0,0 +1,104 @@
+import java.util.*;
+import java.io.*;
+
+public class Simulation_G4_14502 {
+
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+    static int N, M, max;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        // 벽 설치
+        int r1, r2, r3, c1, c2, c3;
+        for (int i = 0; i < N * M; i++) {
+            r1 = i / M;
+            c1 = i % M;
+            if (map[r1][c1] != 0) continue;
+            map[r1][c1] = 1;
+
+            for (int j = 0; j < N * M; j++) {
+                r2 = j / M;
+                c2 = j % M;
+                if (map[r2][c2] != 0) continue;
+                map[r2][c2] = 1;
+
+                for (int k = 0; k < N * M; k++) {
+                    r3 = k / M;
+                    c3 = k % M;
+                    if (map[r3][c3] != 0) continue;
+                    map[r3][c3] = 1;
+                    bfs();
+                    map[r3][c3] = 0;
+                }
+                map[r2][c2] = 0;
+            }
+            map[r1][c1] = 0;
+        }
+
+        System.out.println(max);
+    }
+
+    // 바이러스 전염
+    private static void bfs() {
+        int[][] virusMap = new int[N][M];
+        Queue<int[]> q = new LinkedList<>();
+
+        // 바이러스 전염를 위한 새 배열 생성
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                virusMap[i][j] = map[i][j];
+                // 바이러스 전염을 위한 배열 저장
+                if (virusMap[i][j] == 2) q.offer(new int[] {i, j});
+            }
+        }
+        
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            
+            for (int i = 0; i < 4; i++) {
+                int nr = now[0] + dr[i];
+                int nc = now[1] + dc[i];
+
+                if (isNotInMap(nr, nc) || virusMap[nr][nc] != 0) continue;
+            
+                // 전염
+                q.add(new int[] {nr, nc});
+                virusMap[nr][nc] = 2;
+            }
+        }
+
+        countSpace(virusMap);
+    }
+
+    // 맵 in 여부
+    private static boolean isNotInMap(int r, int c) {
+        return r < 0 || r >= N || c < 0 || c >= M;
+    }
+
+    // 안전공간 카운트
+    private static void countSpace(int[][] virusMap) {
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (virusMap[i][j] == 0) cnt++;
+            }
+        }
+        // 많은 안전공간 새로고침
+        max = Math.max(max, cnt);
+    }
+}
